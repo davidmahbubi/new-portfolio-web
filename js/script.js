6a51b4465f8f1a9342ed1c3b723636dc6b91c938
David Mahbubi <ulrichdavid0370@gmail.com>
2020-07-09 21:32:49 +0700

    Auto hide navbar when page first load in mobile size

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const sidebarDarkOverlay = document.querySelector('.sidebar-dark-overlay');
 const sidebar = document.querySelector('.sidebar');
 const sidebarToggler = document.querySelector('.sidebar-toggler');
 
+if (window.innerWidth <= 1024) {
+    toggleSidebar(sidebar, sidebarDarkOverlay, 'hide');
+}
+
 sidebarDarkOverlay.addEventListener('click', function() {
     toggleSidebar(sidebar, sidebarDarkOverlay, 'hide');
 });
